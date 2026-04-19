7db53c117d65761703d48f767f8de91e3012f80f
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-19 00:27:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const title = document.getElementById("title");
 const button = document.getElementById("changeBtn");
 const toggleButton = document.getElementById("toggleImage");
+const toggleButton = document.getElementById("toggleImage");
  
 button.addEventListener("click", () => {
   title.textContent = "Hello from JavaScript!";
