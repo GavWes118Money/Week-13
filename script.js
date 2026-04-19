077017e36813a400579ac77e4501991e2055d32a
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-19 00:37:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const title = document.getElementById("title");
-const button = document.getElementById("changeBtn");
-const button = document.getElementById("toggleImage");
-const button = document.getElementById("dark Mode");
- 
-button.addEventListener("click", () => {
-  title.textContent = "Hello from JavaScript!";
-  title.style.color = "teal";
-});
+
