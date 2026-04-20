a4798572b43deb1637fa8874baae83c88e5035b7
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-19 20:06:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
 //       Object{} that represents the page you see in the web browser and provides you with an API to interact with it. Web browser constructs the DOM when it loads an HTML document, and structures all the elements in a
 //       tree-like representation. JavaScrpit can access the DOM to dynamically change the content, structuree, and style of web page.
 
-document.title = ""
+document.title = "Web interaction"
