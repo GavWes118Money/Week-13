e61286816dea5f14e092ff9d755cfcef6bae7923
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-19 19:59:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // DOM = DOCUMENT OBJECT MODEL
 //       Object{} that represents the page you see in the web browser and provides you with an API to interact with it. Web browser constructs the DOM when it loads an HTML document, and structures all the elements in a
 //       tree-like representation. JavaScrpit can access the DOM to dynamically change the content, structuree, and style of web page.
+
+document.title = ""
